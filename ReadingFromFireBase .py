4e2070f2d3462fb81a17ba693144769e54c8e8c1
SalimanDolev <44755169+SalimanDolev@users.noreply.github.com>
2021-06-05 17:57:05 +0300

    Update ReadingFromFireBase .py
    add comments

--- ReadingFromFireBase .py
+++ ReadingFromFireBase .py
@@ -1,47 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# npm install -g firebase-tools // Run in cmd in windows
-
-# In[ ]:
-
-
-#pip install firebase
-
-
-# In[ ]:
-
-
-#pip install gcloud
-
-
-# In[ ]:
-
-
-#pip install python_jwt
-
-
-# In[ ]:
-
-
-#pip install sseclient
-
-
-# In[ ]:
-
-
-#pip install pycryptodome
-
-
-# In[ ]:
-
-
-#pip install requests-toolbelt
-
-
-# In[2]:
-
-
+# this script download the data from firebase to the computer once every hour.
 from firebase import Firebase
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
@@ -51,18 +8,12 @@ import csv
 import pandas as pd
 import os
 
-
-# In[ ]:
-
-
-#configuration to the database Firebase
 config = {
   "apiKey": "AIzaSyA4fHNEMAnGEEJV87kFC0GHzMMRc0TatuA",
   "authDomain": "proberequest-finalproject.firebaseapp.com",
   "databaseURL": "https://proberequest-finalproject-default-rtdb.firebaseio.com",
   "storageBucket": "proberequest-finalproject.appspot.com"
 }
-
 
 
 firebase = Firebase(config)
@@ -76,12 +27,5 @@ while(True):
     
     for sensor in usersFrom.keys():
         pd.DataFrame.from_dict(usersFrom[sensor], orient ='index').to_csv("DATA/probeRequest"+str(current_time)+".csv",mode='a')
-    time.sleep(3*60)
+    time.sleep(60*60)
         
-
-
-# In[ ]:
-
-
-
-
