dfaf9fafb0fbb5e5d0a88f3727a319c816c3b662
SalimanDolev <44755169+SalimanDolev@users.noreply.github.com>
2021-09-12 16:56:42 +0300

    Update sendProbeRequests.ino

--- sendProbeRequests.ino
+++ sendProbeRequests.ino
@@ -1,3 +1,5 @@
+// This code is used for sending probe requests every second.
+
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -9,12 +11,15 @@
 
 void setup() {
   Serial.begin(115200);
-  // put your setup code here, to run once:
+  // Set the sensor on ap and station mode
   WiFi.mode(WIFI_AP_STA);
 }
 int i = 0;
 void loop() {
+  // Print the mac address of the sensor
   Serial.print(WiFi.macAddress()+"\n");
+  // When scanning for wifi AP probe requests are sent
   WiFi.scanNetworks();
+  // Delay for one second
   delay(1000);
 }
